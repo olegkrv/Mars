f88ce07c94ae4d6af2e9d13597707e722efd497d
Oleg_Krvs <94847526+olegkrv@users.noreply.github.com>
2022-02-15 20:53:29 +0300

    Update and rename Изображение Марса to Изображение Марса.py

--- Изображение Марса.py
+++ Изображение Марса.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/image_mars')
 def image():
     return f'''<h1>Жди нас, Марс!</h1>
-    <img src="{url_for('static', filename='D:/prohramming/git_ptoject/Mars.png')}">'''
+    <img src="{url_for('static', filename='Mars.png')}">'''
 
 
 if __name__ == '__main__':
