61409d7aa5f40c2389fb52536025ac47a8f7e276
Oleg_Krvs <94847526+olegkrv@users.noreply.github.com>
2022-02-18 20:57:19 +0300

    Update Реклама с картинкой.py

--- Реклама с картинкой.py
+++ Реклама с картинкой.py
@@ -6,13 +6,12 @@ app = Flask(__name__)
 @app.route('/image_sample')
 def image():
     return f'''<h1>Жди нас, Марс!</h1>
-    <img src="{url_for('static', filename='Mars.png')}">
-    <h3 style='color:green'>Тех, кого Земля достала</h3>
-    <h3 style='color:black'>Тех, кто рвётся напролом</h3>
-    <h3 style='color:red'>Ждём у Алого Причала!</h3>
-    <h3 style='color:gray'>Mars_for_people.com</h3>'''
+ <img src="{url_for('static', filename='Mars.png')}">
+ <h3 style='color:green'><p style="background-color: #CFFFBA">Тех, кого Земля достала</p></h3>
+ <h3 style='color:black'><p style="background-color: #BDBDBD">Тех, кто рвётся напролом</p></h3>
+ <h3 style='color:red'><p style="background-color: #FFBABA">Ждём у Алого Причала!</p></h3>
+ <h3 style='color:gray'><p style="background-color: #EFEFEF">Mars_for_people.com</p></h3>'''
 
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
-
